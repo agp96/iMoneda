5cb3afb21f0310b85556f5c7523f7896e4725ede
agp96 <gordoprieto1996>
2019-10-24 19:18:10 +0200

    Commit

--- iMoneda/ViewController.swift
+++ iMoneda/ViewController.swift
@@ -8,13 +8,43 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+enum Tirada {
+    case cara, cruz
+}
 
+class Moneda {
+    var t : Tirada
+    init(t:Tirada) {
+        self.t = t
+    }
+    func lanzar() -> Tirada {
+        return t
+    }
+}
+
+class ViewController: UIViewController {
+    var mon : Moneda
+    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var label: UILabel!
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        <#code#>
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func button(_ sender: Any) {
+        var res = self.mon.lanzar()
+        if res == Tirada.cara {
+        self.label.text = "Cara"
+        }
+        else {
+            self.label.text = "Cruz"
+        }
+    }
+    
 
 }
 
